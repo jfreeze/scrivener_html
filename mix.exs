bbcd0994841dfb309208f3d09fbfbd97cfb96971
Jim <jimfreeze@gmail.com>
2023-11-17 14:34:41 -0600

    update application

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule ScrivenerHtml.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [
-      extra_applications: [:logger, :phoenix_html]
+      extra_applications: [:logger]
     ]
   end
 
